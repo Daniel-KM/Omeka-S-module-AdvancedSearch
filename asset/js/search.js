ee0c08b938717a99870ae5fbdc1e008155f9bc1e
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-03-16 00:00:00 +0100

    Added a check for js chosen.

--- asset/js/search.js
+++ asset/js/search.js
@@ -114,17 +114,18 @@ $(document).ready(function() {
     }
     $('.search-view-type-' + view_type).click();
 
-    /**
-     * Chosen default options.
-     *
-     * @see https://harvesthq.github.io/chosen/
-     */
-    var chosenOptions = {
-        allow_single_deselect: true,
-        disable_search_threshold: 10,
-        width: '100%',
-        include_group_label_in_selected: true,
-    };
-    $('.chosen-select').chosen(chosenOptions);
+    if ($.isFunction($.fn.chosen)) {
+        /**
+         * Chosen default options.
+         * @see https://harvesthq.github.io/chosen/
+         */
+        var chosenOptions = {
+            allow_single_deselect: true,
+            disable_search_threshold: 10,
+            width: '100%',
+            include_group_label_in_selected: true,
+        };
+        $('.chosen-select').chosen(chosenOptions);
+    }
 
 });

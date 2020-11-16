d46faa919e21ede57f13b1e930659c4a1598d286
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-11-16 00:00:00 +0000

    Fixed install.

--- data/doctrine-proxies/__CG__SearchEntitySearchIndex.php
+++ data/doctrine-proxies/__CG__SearchEntitySearchIndex.php
@@ -12,14 +12,14 @@ class SearchIndex extends \Search\Entity\SearchIndex implements \Doctrine\ORM\Pr
      *      three parameters, being respectively the proxy object to be initialized, the method that triggered the
      *      initialization process and an array of ordered parameters that were passed to that method.
      *
-     * @see \Doctrine\Common\Persistence\Proxy::__setInitializer
+     * @see \Doctrine\Common\Proxy\Proxy::__setInitializer
      */
     public $__initializer__;
 
     /**
      * @var \Closure the callback responsible of loading properties that need to be copied in the cloned object
      *
-     * @see \Doctrine\Common\Persistence\Proxy::__setCloner
+     * @see \Doctrine\Common\Proxy\Proxy::__setCloner
      */
     public $__cloner__;
 
@@ -31,20 +31,22 @@ class SearchIndex extends \Search\Entity\SearchIndex implements \Doctrine\ORM\Pr
     public $__isInitialized__ = false;
 
     /**
-     * @var array properties to be lazy loaded, with keys being the property
-     *            names and values being their default values
-     *
-     * @see \Doctrine\Common\Persistence\Proxy::__getLazyProperties
+     * @var array<string, null> properties to be lazy loaded, indexed by property name
      */
-    public static $lazyPropertiesDefaults = [];
-
-
+    public static $lazyPropertiesNames = array (
+);
 
     /**
-     * @param \Closure $initializer
-     * @param \Closure $cloner
+     * @var array<string, mixed> default values of properties to be lazy loaded, with keys being the property names
+     *
+     * @see \Doctrine\Common\Proxy\Proxy::__getLazyProperties
      */
-    public function __construct($initializer = null, $cloner = null)
+    public static $lazyPropertiesDefaults = array (
+);
+
+
+
+    public function __construct(?\Closure $initializer = null, ?\Closure $cloner = null)
     {
 
         $this->__initializer__ = $initializer;
@@ -82,7 +84,7 @@ class SearchIndex extends \Search\Entity\SearchIndex implements \Doctrine\ORM\Pr
 
                 $existingProperties = get_object_vars($proxy);
 
-                foreach ($proxy->__getLazyProperties() as $property => $defaultValue) {
+                foreach ($proxy::$lazyPropertiesDefaults as $property => $defaultValue) {
                     if ( ! array_key_exists($property, $existingProperties)) {
                         $proxy->$property = $defaultValue;
                     }
@@ -165,6 +167,7 @@ class SearchIndex extends \Search\Entity\SearchIndex implements \Doctrine\ORM\Pr
     /**
      * {@inheritDoc}
      * @internal generated method: use only when explicitly handling proxy specific loading logic
+     * @deprecated no longer in use - generated code now relies on internal components rather than generated public API
      * @static
      */
     public function __getLazyProperties()
@@ -257,23 +260,23 @@ class SearchIndex extends \Search\Entity\SearchIndex implements \Doctrine\ORM\Pr
     /**
      * {@inheritDoc}
      */
-    public function getCreated()
-    {
-
-        $this->__initializer__ && $this->__initializer__->__invoke($this, 'getCreated', []);
-
-        return parent::getCreated();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public function setCreated(\DateTime $created)
     {
 
         $this->__initializer__ && $this->__initializer__->__invoke($this, 'setCreated', [$created]);
 
         return parent::setCreated($created);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCreated()
+    {
+
+        $this->__initializer__ && $this->__initializer__->__invoke($this, 'getCreated', []);
+
+        return parent::getCreated();
     }
 
     /**

e21a6291fd711a0ac815941e6fd85c922c494faf
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-08-16 00:00:00 +0000

    Improved element DataTextarea to get sub-values.

--- src/Form/Element/DataTextarea.php
+++ src/Form/Element/DataTextarea.php
@@ -12,6 +12,11 @@ class DataTextarea extends ArrayTextarea
     protected $dataKeys = [];
 
     /**
+     * @var array
+     */
+    protected $dataArrayKeys = [];
+
+    /**
      * @param array $options
      */
     public function setOptions($options)
@@ -19,6 +24,9 @@ class DataTextarea extends ArrayTextarea
         parent::setOptions($options);
         if (array_key_exists('data_keys', $this->options)) {
             $this->setDataKeys($this->options['data_keys']);
+        }
+        if (array_key_exists('data_array_keys', $this->options)) {
+            $this->setDataArrayKeys($this->options['data_array_keys']);
         }
         return $this;
     }
@@ -28,15 +36,27 @@ class DataTextarea extends ArrayTextarea
         if (is_string($array)) {
             return $array;
         }
+        // Reorder values according to specified keys and fill empty values.
         $string = '';
-        foreach ($array as $values) {
-            // Reorder values according to specified keys and fill empty values.
-            $data = count($this->dataKeys)
-                // Associative array.
-                ? array_replace($this->dataKeys, $values)
-                // Simple list.
-                : array_values($values);
-            $string .= implode($this->keyValueSeparator, array_map('strval', $data)) . "\n";
+        $countDataKeys = count($this->dataKeys);
+        // Associative array.
+        if ($countDataKeys) {
+            $arrayKeys = array_intersect_key($this->dataArrayKeys, $this->dataKeys);
+            foreach ($array as $values) {
+                $data = array_replace($this->dataKeys, $values);
+                // Manage sub-values.
+                foreach ($arrayKeys as $arrayKey => $arraySeparator) {
+                    $data[$arrayKey] = implode(' ' . $arraySeparator . ' ', array_map('strval', isset($data[$arrayKey]) ? (array) $data[$arrayKey] : []));
+                }
+                $string .= implode(' ' . $this->keyValueSeparator . ' ', array_map('strval', $data)) . "\n";
+            }
+        }
+        // Simple list.
+        else {
+            foreach ($array as $values) {
+                $data = array_values($values);
+                $string .= implode(' ' . $this->keyValueSeparator . ' ', array_map('strval', $data)) . "\n";
+            }
         }
         return $string;
     }
@@ -47,26 +67,38 @@ class DataTextarea extends ArrayTextarea
             return $string;
         }
         $array = [];
-        $count = count($this->dataKeys);
-        foreach ($this->stringToList($string) as $values) {
-            if ($count) {
+        $countDataKeys = count($this->dataKeys);
+        if ($countDataKeys) {
+            $arrayKeys = array_intersect_key($this->dataArrayKeys, $this->dataKeys);
+            foreach ($this->stringToList($string) as $values) {
                 // Set keys to each part of the line.
                 $keys = array_keys($this->dataKeys);
-                $values = array_map('trim', explode($this->keyValueSeparator, $values, count($this->dataKeys)));
+                $values = array_map('trim', explode($this->keyValueSeparator, $values, $countDataKeys));
                 // Add empty missing values. The number cannot be higher.
                 // TODO Use substr_count() if quicker.
-                $missing = count($this->dataKeys) - count($values);
+                $missing = $countDataKeys - count($values);
                 if ($missing) {
                     $values = array_merge($values, array_fill(0, $missing, ''));
                 }
                 $data = array_combine($keys, $values);
-            } else {
+                // Manage sub-values.
+                foreach ($arrayKeys as $arrayKey => $arraySeparator) {
+                    $data[$arrayKey] = $data[$arrayKey] === ''
+                        ? []
+                        : array_map('trim', explode($arraySeparator, $data[$arrayKey]));
+                }
+                $this->asKeyValue
+                    ? $array[reset($data)] = $data
+                    : $array[] = $data;
+            }
+        } else {
+            foreach ($this->stringToList($string) as $values) {
                 // No keys: a simple list.
                 $data = array_map('trim', explode($this->keyValueSeparator, $values));
+                $this->asKeyValue
+                    ? $array[reset($data)] = $data
+                    : $array[] = $data;
             }
-            $this->asKeyValue
-                ? $array[reset($data)] = $data
-                : $array[] = $data;
         }
         return $array;
     }
@@ -80,7 +112,7 @@ class DataTextarea extends ArrayTextarea
      * With option "as_key_value", the first value will be the used as key for
      * the main array too.
      */
-    public function setDataKeys(array $dataKeys): self
+    public function setDataKeys(array $dataKeys)
     {
         $this->dataKeys = array_fill_keys($dataKeys, null);
         return $this;
@@ -92,5 +124,27 @@ class DataTextarea extends ArrayTextarea
     public function getDataKeys(): array
     {
         return aray_keys($this->dataKeys);
+    }
+
+    /**
+     * Set the option to separate values into multiple values.
+     *
+     * It should be a subset of the data keys.
+     *
+     * It is not recommended to set the first key when option "as_key_value" is
+     *  set. In that case, the whole value is used as key before to be splitted.
+     */
+    public function setDataArrayKeys(array $dataArrayKeys)
+    {
+        $this->dataArrayKeys = $dataArrayKeys;
+        return $this;
+    }
+
+    /**
+     * Get the option to separate values into multiple values.
+     */
+    public function getDataArrayKeys(): array
+    {
+        return $this->dataArrayKeys;
     }
 }

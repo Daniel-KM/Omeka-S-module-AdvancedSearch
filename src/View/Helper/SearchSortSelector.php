02bf23092c1dd54d8454fb46d20e057a5fe84d69
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-03-21 00:00:00 +0000

    Updated sort-selector template.

--- src/View/Helper/SearchSortSelector.php
+++ src/View/Helper/SearchSortSelector.php
@@ -14,78 +14,83 @@ class SearchSortSelector extends AbstractHelper
     protected $partial = 'search/sort-selector';
 
     /**
-     * Option $asSortUrls allows to include the select in the main form or not.
+     * Option $asUrl allows to include the select in the main form or not.
      * When set, a js reloads the page directly.
      * Anyway, the js can rebuild the url from the values.
+     * @todo To avoid a js, a css select (replace select by ul/li + a) can be created in theme.
      *
      * @todo Merge with Omeka SortSelector and SortLink?
      */
-    public function __invoke(Query $query, array $sortOptions, $asSortUrls = false, ?string $partial = null): string
+    public function __invoke(Query $query, array $options, $asUrl = false, ?string $partial = null): string
     {
-        if (empty($sortOptions)) {
+        if (!count($options)) {
             return '';
         }
 
         /* @deprecated Since 3.5.23.3. Kept for old themes. */
-        if (!is_array(reset($sortOptions))) {
-            foreach ($sortOptions as $name => &$sortOption) {
+        if (!is_array(reset($options))) {
+            foreach ($options as $name => &$sortOption) {
                 $sortOption = ['name' => $name, 'label' => $sortOption];
             }
             unset($sortOption);
         }
 
-        $select = $asSortUrls
-            ? $this->asSortUrls($query, $sortOptions)
-            : $this->asForm($query, $sortOptions);
+        $select = $asUrl
+            ? $this->asUrl($query, $options)
+            : $this->asForm($query, $options);
 
         return $this->getView()->partial($partial ?: $this->partial, [
             'query' => $query,
-            'sortOptions' => $sortOptions,
+            'options' => $options,
+            'select' => $select,
+            'asUrl' => $asUrl,
+            // Deprecated since 3.3.6.12.
+            'sortOptions' => $options,
             'sortSelect' => $select,
-            'asSortUrls' => $asSortUrls,
+            'asSortUrls' => $asUrl,
         ]);
     }
 
-    protected function asForm(Query $query, array $sortOptions): Select
+    protected function asForm(Query $query, array $options): Select
     {
         $plugins = $this->getView()->getHelperPluginManager();
         $translate = $plugins->get('translate');
-        foreach ($sortOptions as $name => &$sortOption) {
+        foreach ($options as $name => &$sortOption) {
             $sortOption = $sortOption['label'] ? $translate($sortOption['label']) : $name;
         }
         unset($sortOption);
-        $sortOptions = array_map($translate, $sortOptions);
+        $options = array_map($translate, $options);
         return (new Select('sort'))
-            ->setValueOptions($sortOptions)
+            ->setValueOptions($options)
             ->setValue($query->getSort())
-            ->setLabel($translate('Sort by'));
+            ->setLabel($translate('Sort by')); // @translate
     }
 
-    protected function asSortUrls(Query $query, array $sortOptions): Select
+    protected function asUrl(Query $query, array $options): Select
     {
         $plugins = $this->getView()->getHelperPluginManager();
+        $params = $plugins->get('params');
         $translate = $plugins->get('translate');
         $serverUrl = $plugins->get('serverUrl');
-        $params = $plugins->get('params');
 
         // Prepare urls directly as values to avoid a click. Use current url for a quick build.
         $currentUrl = strtok($serverUrl(true), '?');
         $currentQuery = $params->fromQuery();
         $currentSort = $query->getSort();
-        $sorts = $sortOptions;
-        $sortOptions = [];
+        $sorts = $options;
+        $options = [];
         $currentSortUrl = null;
         foreach ($sorts as $name => $sortOption) {
             $sortName = $currentUrl . '?' . http_build_query(['sort' => $name] + $currentQuery, '', '&', PHP_QUERY_RFC3986);
             if ($name === $currentSort) {
                 $currentSortUrl = $sortName;
             }
-            $sortOptions[$sortName] = $sortOption['label'] ? $translate($sortOption['label']) : $sortName;
+            $options[$sortName] = $sortOption['label'] ? $translate($sortOption['label']) : $sortName;
         }
 
         return (new Select('sort'))
-            ->setValueOptions($sortOptions)
+            ->setValueOptions($options)
             ->setValue($currentSortUrl)
-            ->setLabel($translate('Sort by'));
+            ->setLabel($translate('Sort by')); // @translate
     }
 }

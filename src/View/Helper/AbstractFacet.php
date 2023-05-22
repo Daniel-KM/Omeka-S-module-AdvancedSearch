93ec9d6859987a64efb739ee14ff81b0fef5280e
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-05-22 00:00:00 +0000

    Fixed order in item sets tree.

--- src/View/Helper/AbstractFacet.php
+++ src/View/Helper/AbstractFacet.php
@@ -62,6 +62,11 @@ class AbstractFacet extends AbstractHelper
     protected $queryBase = [];
 
     /**
+     * @var array
+     */
+    protected $tree;
+
+    /**
      * Create one facet as link, checkbox, select or button.
      *
      * @param array $facetValues Each facet value has two keys: value and count.
@@ -127,7 +132,7 @@ class AbstractFacet extends AbstractHelper
     {
         $isFacetModeDirect = ($options['mode'] ?? '') === 'link';
 
-        $tree = $this->isTree && $facetField === 'item_sets_tree' /*&& in_array($facetField, ['item_set', 'item_set_id']) */
+        $this->tree = $this->isTree && substr($facetField, 0, 14) === 'item_sets_tree' /*&& in_array($facetField, ['item_set', 'item_set_id']) */
             ? $this->itemSetsTreeQuick()
             : null;
 
@@ -163,11 +168,39 @@ class AbstractFacet extends AbstractHelper
         }
         unset($facetValue);
 
+        // For item set tree, the values should be reordered according to the
+        // tree, else indentation will be incorrect.
+        if ($this->isTree && count($this->tree) > 1) {
+            $facetValuesByIds = [];
+            foreach ($facetValues as $data) {
+                $facetValuesByIds[$data['value']] = $data;
+            }
+            $facetValues = array_replace($this->tree, $facetValuesByIds);
+            $facetValues = array_intersect_key($facetValues, $facetValuesByIds);
+            /*
+            // Keep added nodes from the tree, so use same keys.
+            // Normally useless if indexed recursively.
+            foreach ($facetValues as &$facetValue) {
+                if (array_key_exists('level', $facetValue)) {
+                    $facetValue = [
+                        'value' => $facetValue['id'],
+                        'count' => 0,
+                        'label' => $facetValue['title'],
+                        'active' => false,
+                        'url' => null,
+                    ];
+                }
+            }
+            unset($facetValue);
+            */
+            $facetValues = array_values($facetValues);
+        }
+
         return [
             'name' => $facetField,
             'facetValues' => $facetValues,
             'options' => $options,
-            'tree' => $this->isTree ? $tree : null,
+            'tree' => $this->isTree ? $this->tree : null,
         ];
     }
 
@@ -293,6 +326,17 @@ class AbstractFacet extends AbstractHelper
                     // In public side, the item set should belong to a site too.
                     : null;
 
+            case 'item_sets_tree':
+            case 'item_sets_tree_is':
+                if (is_numeric($value)) {
+                    return $this->tree[$value]['title'] ?? $value;
+                }
+                // Confirm that the title exists.
+                // This is useless for now, since item sets tree are indexed by id.
+                $labels = array_column($this->tree, 'title', 'id');
+                $key = array_search($value, $labels);
+                return $key ? $value : $value;
+
             case 'property':
             default:
                 return $value;
@@ -307,6 +351,8 @@ class AbstractFacet extends AbstractHelper
      * @see \AdvancedSearch\View\Helper\AbstractFacet::itemsSetsTreeQuick()
      * @see \SearchSolr\ValueExtractor\AbstractResourceEntityValueExtractor::itemSetsTreeQuick()
      *
+     * @todo Simplify ordering: by sql (for children too) or store.
+     *
      * @return array
      */
     protected function itemSetsTreeQuick(): array
@@ -320,6 +366,11 @@ class AbstractFacet extends AbstractHelper
         /** @var \Doctrine\DBAL\Connection $connection */
         $connection = $this->api->read('vocabularies', 1)->getContent()->getServiceLocator()->get('Omeka\Connection');
 
+        $sortingMethod = $this->getView()->setting('itemsetstree_sorting_method', 'title') === 'rank' ? 'rank' : 'title';
+        $sortingMethodSql = $sortingMethod === 'rank'
+            ? 'item_sets_tree_edge.rank'
+            : 'resource.title';
+
         // TODO Use query builder.
         $sql = <<<SQL
 SELECT
@@ -331,9 +382,19 @@ SELECT
 FROM item_sets_tree_edge
 JOIN resource ON resource.id = item_sets_tree_edge.item_set_id
 WHERE item_sets_tree_edge.item_set_id IN (:ids)
-ORDER BY item_sets_tree_edge.item_set_id;
+GROUP BY resource.id
+ORDER BY $sortingMethodSql;
 SQL;
         $flatTree = $connection->executeQuery($sql, ['ids' => array_keys($itemSetTitles)], ['ids' => $connection::PARAM_INT_ARRAY])->fetchAllAssociativeIndexed();
+
+        // Use integers or string to simplify comparaisons.
+        foreach ($flatTree as &$node) {
+            $node['id'] = (int) $node['id'];
+            $node['parent'] = (int) $node['parent'] ?: null;
+            $node['rank'] = (int) $node['rank'];
+            $node['title'] = (string) $node['title'];
+        }
+        unset($node);
 
         $structure = [];
         foreach ($flatTree as $id => $node) {
@@ -348,6 +409,9 @@ SQL;
             while ($parentId = $nodeWhile['parent']) {
                 $ancestors[$parentId] = $parentId;
                 $nodeWhile = $flatTree[$parentId] ?? null;
+                if (!$nodeWhile) {
+                    break;
+                }
             }
             $structure[$id] = $node;
             $structure[$id]['children'] = $children;
@@ -355,8 +419,93 @@ SQL;
             $structure[$id]['level'] = count($ancestors);
         }
 
+        // Order by sorting method.
+        if ($sortingMethod === 'rank') {
+            $sortingFunction = function ($a, $b) use ($structure) {
+                return $structure[$a]['rank'] - $structure[$b]['rank'];
+            };
+        } else {
+            $sortingFunction = function ($a, $b) use ($structure) {
+                return strcmp($structure[$a]['title'], $structure[$b]['title']);
+            };
+        }
+
+        foreach ($structure as &$node) {
+            usort($node['children'], $sortingFunction);
+        }
+        unset($node);
+
+        // Get and order root nodes.
+        $roots = [];
+        foreach ($structure as $id => $node) {
+            if (!$node['level']) {
+                $roots[$id] = $node;
+            }
+        }
+
+        // Root is already ordered via sql.
+
+        // Reorder whole structure.
+        // TODO Use a while loop.
+        $result = [];
+        foreach ($roots as $id => $root) {
+            $result[$id] = $root;
+            foreach ($root['children'] ?? [] as $child1) {
+                $child1 = $structure[$child1];
+                $result[$child1['id']] = $child1;
+                foreach ($child1['children'] ?? [] as $child2) {
+                    $child2 = $structure[$child2];
+                    $result[$child2['id']] = $child2;
+                    foreach ($child2['children'] ?? [] as $child3) {
+                        $child3 = $structure[$child3];
+                        $result[$child3['id']] = $child3;
+                        foreach ($child3['children'] ?? [] as $child4) {
+                            $child4 = $structure[$child4];
+                            $result[$child4['id']] = $child4;
+                            foreach ($child4['children'] ?? [] as $child5) {
+                                $child5 = $structure[$child5];
+                                $result[$child5['id']] = $child5;
+                                foreach ($child5['children'] ?? [] as $child6) {
+                                    $child6 = $structure[$child6];
+                                    $result[$child6['id']] = $child6;
+                                    foreach ($child6['children'] ?? [] as $child7) {
+                                        $child7 = $structure[$child7];
+                                        $result[$child7['id']] = $child7;
+                                        foreach ($child7['children'] ?? [] as $child8) {
+                                            $child8 = $structure[$child8];
+                                            $result[$child8['id']] = $child8;
+                                            foreach ($child8['children'] ?? [] as $child9) {
+                                                $child9 = $structure[$child9];
+                                                $result[$child9['id']] = $child9;
+                                                foreach ($child9['children'] ?? [] as $child10) {
+                                                    $child10 = $structure[$child10];
+                                                    $result[$child10['id']] = $child10;
+                                                    foreach ($child10['children'] ?? [] as $child11) {
+                                                        $child11 = $structure[$child11];
+                                                        $result[$child11['id']] = $child11;
+                                                        foreach ($child11['children'] ?? [] as $child12) {
+                                                            $child12 = $structure[$child12];
+                                                            $result[$child12['id']] = $child12;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        $structure = $result;
+
         // Append missing item sets.
         foreach (array_diff_key($itemSetTitles, $flatTree) as $id => $title) {
+            if (isset($structure[$id])) {
+                continue;
+            }
             $structure[$id] = [
                 'id' => $id,
                 'parent' => null,

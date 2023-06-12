5ca1a0f72385f1f9b9cbbf7935207ab3bdb35980
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-06-12 00:00:00 +0000

    Fixed notice.

--- src/View/Helper/AbstractFacetElement.php
+++ src/View/Helper/AbstractFacetElement.php
@@ -14,7 +14,7 @@ class AbstractFacetElement extends AbstractFacet
      * May contain the key, that may be a numeric key, or string like "from" or "to".
      * @return string|array
      */
-    public function __invoke(string $facetField, array $facet, array $options = [], bool $asData = false)
+    public function __invoke(?string $facetField, array $facet, array $options = [], bool $asData = false)
     {
         // Variables are static to speed up process for all facets.
         // TODO Share the list between active and facet helpers.

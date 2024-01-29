6c245c254a13e1b466ef1b18f81fa4013a92d49c
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-01-29 00:00:00 +0000

    Released version 3.4.20.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -435,3 +435,18 @@ WHERE `id` = "advancedsearch_restrict_used_terms";
 SQL;
     $connection->executeStatement($sql);
 }
+
+if (version_compare($oldVersion, '3.4.20', '<')) {
+    if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.49')) {
+        $message = new Message(
+            'The module %1$s should be upgraded to version %2$s or later.', // @translate
+            'Common', '3.4.49'
+        );
+        throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+    }
+
+    $message = new Message(
+        'When full text is managed in alto files, it is now possible to search full text or record only.' // @translate
+    );
+    $messenger->addSuccess($message);
+}
